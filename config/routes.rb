eab118b42eab869356615628f1197724eace41ad
sothea578 <thea578@gmail.com>
2017-07-24 08:53:28 +0700

    Sign In and Sign Up

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users
   root "static_pages#index"
   get "index", to: "static_pages#index"
   get "/home", to: "static_pages#home"
   get "/artist", to: "static_pages#artist"
   get "/track", to: "static_pages#track"
+  resources :users, only: [:index, :show, :edit, :update]
 end
